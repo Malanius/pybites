a1a90b1adefcd77fe150010bad5934c6618b18a4
Malanius Privierre <malaniusprivierre@gmail.com>
2022-09-09 08:56:35 +0200

    :white_check_mark: (regular): Solve bite 149

---
+++ pybites/regular_bites/149/words.py
@@ -0,0 +1,9 @@
+from typing import List
+
+
+def sort_words_case_insensitively(words: List[str]) -> List[str]:
+    """Sort the provided word list ignoring case, and numbers last
+    (1995, 19ab = numbers / Happy, happy4you = strings, hence for
+     numbers you only need to check the first char of the word)
+    """
+    return sorted(words, key=lambda x: (x[0].isdigit(), x.lower()))
