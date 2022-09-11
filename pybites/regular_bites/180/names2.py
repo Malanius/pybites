9d8a1312ab827772c9e357295ac75bf194f7ea14
Malanius Privierre <malaniusprivierre@gmail.com>
2022-09-11 10:20:08 +0200

    :white_check_mark: (regular): Solve bite 180

---
+++ pybites/regular_bites/180/names2.py
@@ -0,0 +1,22 @@
+from collections import defaultdict
+
+# fake data from https://www.mockaroo.com
+data = """last_name,first_name,country_code
+Watsham,Husain,ID
+Harrold,Alphonso,BR
+Apdell,Margo,CN
+Tomblings,Deerdre,RU
+Wasielewski,Sula,ID
+Jeffry,Rudolph,TD
+Brenston,Luke,SE
+Parrett,Ines,CN
+Braunle,Kermit,PL
+Halbard,Davie,CN"""
+
+
+def group_names_by_country(data: str = data) -> defaultdict:
+    countries = defaultdict(list)
+    for line in data.splitlines()[1:]:
+        firs_name, last_name, country_code = line.split(",")
+        countries[country_code].append(last_name + " " + firs_name)
+    return countries
