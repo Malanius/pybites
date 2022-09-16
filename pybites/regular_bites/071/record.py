97536e2f52cbb2636050a8003e6f07a1f5ae4959
Malanius Privierre <malaniusprivierre@gmail.com>
2022-09-16 09:23:00 +0200

    :white_check_mark: (regular, oop-path): Solve bite 071

---
+++ pybites/regular_bites/071/record.py
@@ -0,0 +1,10 @@
+class RecordScore:
+    """Class to track a game's maximum score"""
+
+    def __init__(self):
+        self.max_score = None
+
+    def __call__(self, score):
+        if self.max_score is None or score > self.max_score:
+            self.max_score = score
+        return self.max_score
