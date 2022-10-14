81f52fed1954456e11a270dfa3d7b45f71968984
Malanius Privierre <malaniusprivierre@gmail.com>
2022-10-14 18:42:43 +0200

    :white_check_mark: (regular, itertools-path): Solve bite 066

---
+++ pybites/regular_bites/066/running_mean.py
@@ -0,0 +1,9 @@
+import itertools as it
+
+
+def running_mean(sequence):
+    """Calculate the running mean of the sequence passed in,
+    returns a sequence of same length with the averages.
+    You can assume all items in sequence are numeric."""
+    for i, num in enumerate(it.accumulate(sequence), start=1):
+        yield round(num / i, 2)
