43f48e4a704086ecd086d05e70ee37e71e898bae
Malanius Privierre <malaniusprivierre@gmail.com>
2022-11-16 10:14:02 +0100

    :white_check_mark: (data-analysis-path): Solve bite 316

---
+++ pybites/316/movie_budget.py
@@ -0,0 +1,43 @@
+from collections import defaultdict
+from datetime import date
+from typing import Dict, Sequence, NamedTuple
+
+
+class MovieRented(NamedTuple):
+    title: str
+    price: int
+    date: date
+
+
+RentingHistory = Sequence[MovieRented]
+STREAMING_COST_PER_MONTH = 12
+STREAM, RENT = "stream", "rent"
+
+
+def get_month(date: date) -> str:
+    return date.strftime("%Y-%m")
+
+
+def rent_or_stream(
+    renting_history: RentingHistory,
+    streaming_cost_per_month: int = STREAMING_COST_PER_MONTH,
+) -> Dict[str, str]:
+    """Function that calculates if renting movies one by one is
+    cheaper than streaming movies by months.
+
+    Determine this PER MONTH for the movies in renting_history.
+
+    Return a dict of:
+    keys = months (YYYY-MM)
+    values = 'rent' or 'stream' based on what is cheaper
+
+    Check out the tests for examples.
+    """
+    rent_price_per_month = defaultdict(int)
+    for movie in renting_history:
+        rent_price_per_month[get_month(movie.date)] += movie.price
+
+    return {
+        month: RENT if price <= streaming_cost_per_month else STREAM
+        for month, price in rent_price_per_month.items()
+    }
